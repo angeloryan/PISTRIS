d9152dd17343485a9c7ab64e6ec95e0c4899c52f
Rushil <rushilp002@gmail.com>
2021-04-17 23:27:00 -0700

    buffer for health

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import sprites
 import LinkedList
 import random
+pygame.font.init()
+
 
 # Screen Variables
 WIDTH, HEIGHT = 900, 500
@@ -14,7 +16,9 @@ SHARK_WIDTH, SHARK_HEIGHT = 100, 100
 BOTTLE_WIDTH, BOTTLE_HEIGHT = 10, 10
 COLLISION = pygame.USEREVENT + 1
 SCREEN_COLOR = (150, 150, 255)
+WHITE = (255, 255, 255)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+HEALTH_FONT = pygame.font.SysFont('timesnewroman', 40)
 
 SHARK_IMAGE = pygame.image.load(os.path.join('assets', 'shark.png'))
 BOTTLE_IMAGE = pygame.image.load(os.path.join('assets', 'water_bottle.png'))
@@ -24,7 +28,7 @@ SHARK = pygame.transform.scale(SHARK_IMAGE, (100, 100))
 BOTTLES = [BOTTLE_IMAGE, pygame.transform.rotate(BOTTLE_IMAGE, 90), pygame.transform.rotate(BOTTLE_IMAGE, 180), pygame.transform.rotate(BOTTLE_IMAGE, 270)]
 OCEAN = [0, 900 , 1800]
 
-def draw_window(list : LinkedList.LinkedList()):
+def draw_window(list : LinkedList.LinkedList(), shark_hp):
     # WIN.blit(OCEAN_IMAGE, (0, 0))
     for i in range(3):
         WIN.blit(OCEAN_IMAGE, (OCEAN[i], 0))
@@ -45,6 +49,9 @@ def draw_window(list : LinkedList.LinkedList()):
                 curr.data.hitbox.y = (random.randint(10, 450))
         curr = curr.next
 
+        SHOW_AVATAR_HEALTH = HEALTH_FONT.render("Health: " + str(shark_hp),1,WHITE)
+        WIN.blit(SHOW_AVATAR_HEALTH, (10,10))
+
     pygame.display.update()
 
 def handle_avatar_movement(sprite: sprites.Sprites, keys_pressed):
@@ -60,13 +67,14 @@ def handle_avatar_movement(sprite: sprites.Sprites, keys_pressed):
     if keys_pressed[pygame.K_RIGHT] and sprite.get_x() + sprite.hitbox.width < WIDTH:  # RIGHT
         sprite.set_x(PLAYER_SPEED)
 
-def collision(shark, list):
+def collision(shark, list, vulnerable):
     curr = list.head.next
 
-    while curr:
+    while curr and vulnerable:
         if shark.hitbox.colliderect(curr.data.hitbox):
             pygame.event.post(pygame.event.Event(COLLISION))
             print("Hit!")
+            vulnerable = False
 
         curr = curr.next
 
@@ -74,8 +82,13 @@ def main():
     run = True
     shark_hp = 10
     time = 0
+    counter = 0
     clock = pygame.time.Clock()
     list = LinkedList.LinkedList()
+    vulnerable = True
+    
+    
+
 
     shark = sprites.Sprites(SHARK, pygame.Rect(10, 300, SHARK_WIDTH, SHARK_HEIGHT))
 
@@ -89,6 +102,11 @@ def main():
         clock.tick(FPS)
         time += 1
 
+        if vulnerable is False:
+            counter += 1
+        if counter % 30 == 0:
+            vulnerable = True
+
         # Scrolling background, resets background ahead if background hits fully offscreen
         for i in range(3):
             OCEAN[i] -= BACKGROUND_SPEED
@@ -101,13 +119,14 @@ def main():
                 run = False
             if event.type == COLLISION:
                 shark_hp -= 1
+                vulnerable = False
         if shark_hp <= 0:
             break
         
         keys_pressed = pygame.key.get_pressed()
         handle_avatar_movement(shark, keys_pressed)
-        collision(shark, list)
-        draw_window(list)
+        collision(shark, list, vulnerable)
+        draw_window(list, shark_hp)
 
     pygame.QUIT()
 
