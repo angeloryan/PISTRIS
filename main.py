b0414fa8d6cc985ba0780a6cea97d028f57107f9
Rushil <rushilp002@gmail.com>
2021-04-17 21:45:43 -0700

    added health hi

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import os
 import sprites
 import LinkedList
 import random
+pygame.font.init()
+pygame.mixer.init()
 
 # Screen Variables
 WIDTH, HEIGHT = 900, 500
 FPS = 60
 SPEED = 5
+WHITE = (255, 255, 255)
 SHARK_WIDTH, SHARK_HEIGHT = 100, 100
 BOTTLE_WIDTH, BOTTLE_HEIGHT = 10, 10
 COLLISION = pygame.USEREVENT + 1
 SCREEN_COLOR = (150, 150, 255)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
 
 SHARK_IMAGE = pygame.image.load(os.path.join('assets', 'shark.png'))
 BOTTLE_IMAGE = pygame.image.load(os.path.join('assets', 'water_bottle.png'))
@@ -21,11 +25,20 @@ OCEAN_IMAGE = pygame.image.load(os.path.join('assets', 'ocean.png'))
 
 
 # 1 = Up, 2 = Right, 3 = Down, 4 = Left
-SHARK = pygame.transform.scale(SHARK_IMAGE, (100, 100))
-BOTTLES = [BOTTLE_IMAGE, pygame.transform.rotate(BOTTLE_IMAGE, 90), pygame.transform.rotate(BOTTLE_IMAGE, 180), pygame.transform.rotate(BOTTLE_IMAGE, 270)]
 
-def draw_window(list : LinkedList.LinkedList()):
+BOTTLE_1 = BOTTLE_IMAGE
+BOTTLE_2 = pygame.transform.rotate(BOTTLE_IMAGE, 90)
+BOTTLE_3 = pygame.transform.rotate(BOTTLE_IMAGE, 180)
+BOTTLE_4 = pygame.transform.rotate(BOTTLE_IMAGE, 270)
+SHARK = pygame.transform.scale(SHARK_IMAGE, (100, 100))
+
+bottle = [BOTTLE_1, BOTTLE_2, BOTTLE_3, BOTTLE_4]
+
+def draw_window(list : LinkedList.LinkedList(), shark_hp):
     WIN.blit(OCEAN_IMAGE, (0, 0))
+
+    SHOW_AVATAR_HEALTH = HEALTH_FONT.render("Health: " + str(shark_hp), 1, WHITE)
+    WIN.blit(SHOW_AVATAR_HEALTH, (10, 10))
 
     curr = list.head
 
@@ -60,15 +73,15 @@ def collision(shark, list):
 
 def main():
     run = True
-    shark_hp = 10
+    shark_hp = 100
     clock = pygame.time.Clock()
     list = LinkedList.LinkedList()
 
     shark = sprites.Sprites(SHARK, pygame.Rect(10, 300, SHARK_WIDTH, SHARK_HEIGHT))
-    trash = sprites.Sprites(BOTTLES[random.randint(0, 3)], pygame.Rect(random.randint(10, 500), random.randint(10, 500), 10, 10))
+    trash = sprites.Sprites(bottle[random.randint(0, 3)], pygame.Rect(random.randint(10, 500), random.randint(10, 500), 10, 10))
 
     for i in range(10):
-        list.push(sprites.Sprites(BOTTLES[random.randint(0, 3)], pygame.Rect(random.randint(100, 900), random.randint(10, 600), 10, 10)))
+        list.push(sprites.Sprites(bottle[random.randint(0, 3)], pygame.Rect(random.randint(100, 900), random.randint(10, 600), 10, 10)))
 
     list.push(trash)
     list.push(shark)
@@ -81,14 +94,15 @@ def main():
                 run = False
             if event.type == COLLISION:
                 shark_hp -= 1
+
         if shark_hp <= 0:
-            print("dead!")
+            print("working!")
             break
         
         keys_pressed = pygame.key.get_pressed()
         handle_avatar_movement(shark, keys_pressed)
         collision(shark, list)
-        draw_window(list)
+        draw_window(list, shark_hp)
 
     pygame.QUIT()
 
